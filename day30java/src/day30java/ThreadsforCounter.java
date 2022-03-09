78d0ec0e47c3230d772ee1eef32940a7c4c08c5c
ruman <mohammedruman1996@gmail.com>
2022-03-09 14:56:36 +0530

    updated

--- day30java/src/day30java/ThreadsforCounter.java
+++ day30java/src/day30java/ThreadsforCounter.java
@@ -1,16 +1,36 @@
 package day30java;
 
+class ThreadsforCounter1 extends Thread {
+	public void run ( ) {
+		int j = 0;
+		while (true) {
+			System.out.println("current token of Counter1 is :" + j);
+			j++;
+			if (j==5000) {
+				j = 0 ;
+				try {
+					Thread.sleep(10000);
+					}
+				catch (Exception e)  
+				{  
+				// catching the exception  
+					System.out.println(e);  
+				}
+			}
+		}
+	}
+}
 public class ThreadsforCounter implements Runnable { 
 
 	  public void run() {
 		  int i=0;
 			  while (true) {  
-		 System.out.println("current Token of Counter :" + i);
+		 System.out.println("current Token of Counter is :" + i);
 		  i++;
 			  if (i==5000) {		
 			  i=0;
 			  try {
-					Thread.sleep(1000);
+					Thread.sleep(10000);
 					}
 				catch (Exception e)  
 				{  
@@ -23,8 +43,9 @@ public class ThreadsforCounter implements Runnable {
 	  public static void main(String[] args) {
 			// TODO Auto-generated method stub
 		  ThreadsforCounter tc = new ThreadsforCounter();
+		  ThreadsforCounter1 tc1 = new ThreadsforCounter1();
 			Thread t1 =new Thread (tc);
-			Thread t2 = new Thread (tc);
+			Thread t2 = new Thread (tc1);
 	        
 	         t1.start();
 	         t2.start();
